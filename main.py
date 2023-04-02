ad1b8b2a81d17cf38dab23eca2b31d4c887f833d
Виктор Горбач <viktor.gorbatch@yandex.ru>
2023-04-02 21:06:32 +0300

    Code.Style: practic

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+"""The best game in the world."""
 from random import randint
+
 from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Another function for attack another gamers."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Another function for defence from another gamers."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -24,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Dangerous function for using ultimate skils."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -34,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Dangerous place. The champions are crying."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -58,6 +64,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Chose your destiny."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -81,6 +88,10 @@ def choice_char_class() -> str:
 
 
 def main() -> None:
+    """Fight.
+    Naming
+    Taking
+    """
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
